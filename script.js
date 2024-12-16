045ff244a2f97a54047022207a953ac01c28d013
Dani Tadesse <105855741+danitadesse@users.noreply.github.com>
2024-12-16 10:28:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,22 +3,9 @@ window.addEventListener("load", () => {
   document.body.classList.add("loaded");
 });
 
-// Smooth scroll to top
-const backToTopButton = document.querySelector('.back-to-top');
-backToTopButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-});
-
 // JavaScript to handle the hamburger menu toggle
-const menuIcon = document.querySelector("#menu-icon");
-const navbar = document.querySelector(".navbar");
-const navbg = document.querySelector(".nav-bg");
-menuIcon.addEventListener("click", () => {
-  menuIcon.classList.toggle("bx-x");
-  navbar.classList.toggle("active");
-  navbg.classList.toggle("active");
-});
+humbr = document.querySelector(".hum");
+humbr.onclick = function () {
+  navBar = document.querySelector(".nav-bar");
+  navBar.classList.toggle("active");
+};
